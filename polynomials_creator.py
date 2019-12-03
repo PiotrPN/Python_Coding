525c120b9a5da6eceb953bf801ddfb806b275a03
PiotrPN <51441857+piotrpn@users.noreply.github.com>
2019-12-03 11:58:10 +0100

    Function

---
+++ polynomials_creator.py
@@ -0,0 +1,10 @@
+def polynomials(*args):
+    a=len(args)
+    f=f"{args[0]}*x**({a-1})"
+    if a<=1:
+        return args[0]
+    else:
+        for n,i in zip(args[1:],range(2,a+1)):
+            f+=f"+({n})*x**{a-i}"
+    return f
+print(polynomials(8))
