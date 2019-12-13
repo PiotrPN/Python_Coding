7874ead3ddca7671b25abd2ce6949191410665ab
Piotr N <pniedzwiecki@yandex.com>
2019-12-13 02:40:56 +0100

    Used yield generator

--- Project Euler/Euler no4.py
+++ Project Euler/Euler no4.py
@@ -1,9 +1,16 @@
 """project euler Problem 4
 Find the largest palindrome made from the product of two 3-digit numbers."""
-palindrome=set()
-for n in range (100,1000):
-    for m in range(100,1000):
-        numb=str(n*m)
-        if numb==numb[::-1]:
-            palindrome.add(n*m)
-print(max(palindrome))
+def numbers(min,max):
+    for n in range (min,max):
+        for m in range(min,max):
+            numb=n*m
+            yield numb
+
+def biggest_palindrome(min_numb,max_numb):
+    palindrome=0
+    for numb in numbers(min_numb,max_numb):
+        if str(numb)==str(numb)[::-1]:
+            if numb>palindrome:
+                palindrome=numb
+    return palindrome
+print(biggest_palindrome(100,1000))
